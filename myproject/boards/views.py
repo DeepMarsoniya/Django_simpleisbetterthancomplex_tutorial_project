566b7d3a53ee5c1e97b0198a8661630683694be3
Deep Marsoniya <deepmarsoniya96@gmail.com>
2021-07-16 21:49:44 +0530

    T2 - home page with board list

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Board
 
 
 # here in home ide shows warning like request param is unused,
 # but id you will remove it, it will stop working
-# I don't know why but I think it will not match the handler function identity	
+# I don't know why but I think it will not match the handler function identity
 def home(request):
-	return HttpResponse('Hello, World!')
+	boards = Board.objects.all()
+
+
+	return render(request,'home.html',{'boards':boards})
